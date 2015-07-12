999db6622123dabdf33eef5fcbee46a6341d9e38
Tim Green <tiimgreen@gmail.com>
2015-07-12 18:38:39 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # Settings
-  get 'settings',          to: 'site_settings#index',  as: :site_settings
-  get 'settings/:id/edit', to: 'site_settings#edit',   as: :site_setting_edit
-  get 'settings/:id',      to: 'site_settings#show',   as: :site_setting
-  match 'settings/:id',    to: 'site_settings#update', via: :patch
+  get 'settings',          to: 'settings#index',  as: :site_settings
+  get 'settings/:id/edit', to: 'settings#edit',   as: :edit_site_setting
+  get 'settings/:id',      to: 'settings#show',   as: :site_setting
+  match 'settings/:id',    to: 'settings#update', via: :patch
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
