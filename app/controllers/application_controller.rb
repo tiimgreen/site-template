3e53cef111ef63fb19aa1855646df22e5745f0fa
Tim Green <tiimgreen@gmail.com>
2015-07-12 11:40:46 +0100

    Add global helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+    def set_title
+      unless action_name.to_s.downcase == 'destroy'
+
+        case params[:action]
+        when 'index'
+          @page_title = controller_name.titleize
+        when 'show'
+          @page_title = controller_name.singularize.titleize
+        when 'new', 'create'
+          @page_title = "New #{controller_name.singularize.titleize}"
+        when 'edit', 'update'
+          @page_title = "Edit #{controller_name.singularize.titleize}"
+        else
+          @page_title = action_name.titleize
+        end
+      end
+    end
+
+    def render_404
+      raise ActionController::RoutingError.new('Page Not Found')
+    end
 end
