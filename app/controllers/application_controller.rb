bb2de84f1c1ba8e3855bfe792efc5f787932f648
Tim Green <tiimgreen@gmail.com>
2015-07-14 18:49:24 +0100

    Update AdminHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_page
   before_action :set_title
 
   protected
@@ -27,5 +28,9 @@ class ApplicationController < ActionController::Base
 
     def render_404
       raise ActionController::RoutingError.new('Page Not Found')
+    end
+
+    def set_page
+      @page = WebPage.find_or_create_by(key: "#{controller_name}_#{action_name}")
     end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -5,29 +5,35 @@ module AdminHelper
   def edit_text(
     page,
     key,
-    default_text = 'Lorem ipsum dolor sit amet domo, consectetur adipisicing elit.'
+    default_text = 'Lorem ipsum dolor sit amet domo, consectetur adipisicing elit.',
+    options = { render_markdown: true, p_tags: true }
   )
     page_id = key.start_with?('global') ? 0 : page.id
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                        autolink: true,
+                                        space_after_headers: true,
+                                        prettify: true)
 
-    if (page_element = PageElementText.find_by(key: key, web_page_id: page_id)).nil?
-      value_to_return = default_text
+    page_element =
+      PageElementText.create_with(value: default_text).find_or_create_by(key: key, web_page_id: page_id)
 
-      page_element = PageElementText.create(
-        key: key,
-        value: default_text,
-        web_page_id: page_id
-      )
-    else
-      value_to_return = page_element.value
-    end
+    value_to_return = page_element.default_text
 
     if user_signed_in?
       value_to_return += " "
       value_to_return +=
-        link_to('Edit', edit_page_element_path(page_element), class: 'edit-page-element')
+        link_to('Edit', edit_page_element_text_path(page_element), class: 'edit-page-element')
     end
 
-    value_to_return.html_safe
+    if options[:render_markdown]
+      value_to_return = @markdown.render(value_to_return).html_safe
+
+      unless options[:p_tags]
+        value_to_return = Regexp.new(/\A<p>(.*)<\/p>\Z/m).match(value_to_return)[1] rescue value_to_return
+      end
+    else
+      value_to_return = value_to_return.html_safe
+    end
   end
 
   def read_text(
@@ -54,23 +60,28 @@ module AdminHelper
   )
     page_id = key.start_with?('global') ? 0 : page.id
 
-    if (page_element_link = PageElementLink.find_by(key: key, web_page_id: page_id)).nil?
-      value_to_return = link_to(default_text, default_link, options)
+    page_element_link =
+      PageElementLink
+        .create_with(text: default_text, link: default_link)
+        .find_or_create_by(key: key, web_page_id: page_id)
 
-      page_element_link = PageElementLink.create(
-          key: key,
-          text: default_text,
-          link: default_link,
-          web_page_id: page_id
-        )
-    else
-      value_to_return = link_to(page_element_link.text, page_element_link.link, options)
-    end
+    value_to_return = link_to(page_element_link.text, page_element_link.link, options)
 
     if user_signed_in?
       value_to_return += " "
       value_to_return +=
         link_to('Edit', edit_page_element_link_path(page_element_link), class: 'edit-page-element')
+    end
+
+    value_to_return.html_safe
+  end
+
+  def edit_setting(key)
+    setting = Setting.find_by(key: key)
+    value_to_return = setting.value
+
+    if user_signed_in?
+      value_to_return += link_to('Edit', edit_setting_path(setting), class: 'edit-page-element')
     end
 
     value_to_return.html_safe
